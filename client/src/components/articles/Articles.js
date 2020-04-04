7053bf186dbc1ef7d1442ad0b4e98a5232a41b7f
Alexis Lopez <alexis.lopez87@gmail.com>
2020-04-04 16:47:19 -0300

    adding css style

--- client/src/components/articles/Articles.js
+++ client/src/components/articles/Articles.js
@@ -1,9 +1,16 @@
 import React, { Fragment } from 'react';
+import Grid from "@material-ui/core/Grid";
 
 const Articles = (props) => {
     return (
        <Fragment>
-           List of articles
+            <Grid container justify="center" spacing={3}>
+                {[0, 1, 2].map(value => (
+                    <Grid key={value} item md={10}>
+                        probando grid 
+                    </Grid>
+                ))}
+            </Grid>
        </Fragment>
     );
 }
--- client/src/components/layout/Header.js
+++ client/src/components/layout/Header.js
@@ -1,10 +1,26 @@
-import React, { Fragment } from 'react';
+import React from 'react';
+import Grid from "@material-ui/core/Grid";
+
+const classes = {
+    container: {
+      background: "#000",
+      color: 'white'
+    },
+    tile:{
+        fontSize: 50
+    }
+};
 
 const Header = (props) => {
      return (
-        <Fragment>
-            <h1>HN Feed</h1>
-        </Fragment>
+        <Grid container justify="center" spacing={1} style={classes.container} alignItems="flex-start">
+            <Grid key={1} item md={10}>
+                <h1 style={classes.tile}>HN Feed</h1>
+            </Grid>    
+            <Grid key={2} item md={10}>
+                <h3>We {'<3'} hacker news!</h3>
+            </Grid>   
+        </Grid>
      );
 }
 
