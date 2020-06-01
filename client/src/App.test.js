041cfefc9e82e55e1008df31e7a86e0a4ecaebba
Alexis Lopez <alexis.lopez87@gmail.com>
2020-06-01 15:30:42 -0400

    add unit test for frontend

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,9 +1,16 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { mount, shallow } from "enzyme";
 import App from './App';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe("Api Cron Testing", () => {
+
+  let wrapper;
+  beforeEach(() => {
+    wrapper = mount(<App />);
+  });
+
+  test("render the title of the app", () => {
+    expect(wrapper.find("#title").text()).toContain("HN Feed");
+  });
+
 });
--- client/src/components/layout/Header.js
+++ client/src/components/layout/Header.js
@@ -16,7 +16,7 @@ const Header = (props) => {
      return (
         <Grid container justify="center" spacing={1} style={classes.container} alignItems="flex-start">
             <Grid key={1} item md={10}>
-                <h1 style={classes.tile}>HN Feed</h1>
+                <h1 id="title" style={classes.tile}>HN Feed</h1>
             </Grid>    
             <Grid key={2} item md={10}>
                 <h3>We {'<3'} hacker news!</h3>
